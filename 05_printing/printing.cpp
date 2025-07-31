cbe2ae1450c91a5be39f022165272e81994819e1
wildanjr19 <wildansekolah11@gmail.com>
2025-07-31 16:46:12 +0700

    printing

---
+++ 05_printing/printing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// body
+int main()
+{
+    std::cout << "Hello World" << std::endl; // statement
+    std::cout << "Bandung" << std::endl;
+    std::cin.get();                          // statement   
+    return 0;
+}
+
+// cout : console out
+// endl : end line
