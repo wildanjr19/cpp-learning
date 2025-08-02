95993d8f37f1ff26af1ec8b3152f54d576a33b7e
wildanjr19 <wildansekolah11@gmail.com>
2025-08-02 08:09:47 +0700

    data type in cpp

---
+++ 09_tipe_data/tipe_data.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // bilangan bulat
+    int a = 1;
+    long b = 6;
+    short c = 7;
+    
+    // bilangan desimal
+    float d = 1.0;
+    double e = 2.5;
+
+    // character 
+    char f = 'a';
+
+    /// boolean
+    bool g = true;
+
+    cout << a << endl;
+    cout << sizeof(a) << " byte" << endl;
+
+    cin.get();
+    return 0;
+}
