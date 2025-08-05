1a2e228aa563d1b4d0a76f525ebd3f321ddc72eb
wildanjr19 <wildansekolah11@gmail.com>
2025-08-05 09:48:36 +0700

    assignment in cpp

---
+++ 17_assignment/assignment.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 3;
+
+    cout << "Nilai awal dari a : " << a << endl;
+
+    // asignment operator
+    a += 3;
+
+    cout << a << endl;
+
+    cin.get();
+    return 0;
+}
