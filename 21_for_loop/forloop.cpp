583d8923e95673803f584e4e5c24ca65d1b9ca19
wildanjr19 <wildansekolah11@gmail.com>
2025-08-09 19:21:49 +0700

    for loop in cpp

---
+++ 21_for_loop/forloop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /*
+    for (inisialisasi, loop kondisi, increment) {
+        statement
+    }
+    */
+    for( int i = 1; i <= 5; i++) {
+        cout << "iterasi ke-" << i << endl;
+    }
+
+    return 0;
+}
